0636693b28f69ef70759a746f6e7012811eaa9c8
Prateek-VIT <prateek.karn2020@vitstudent.ac.in>
2024-03-13 12:21:33 +0530

    PUshing new changes

---
+++ run_ablation.sh
@@ -0,0 +1,148 @@
+dora grid nmi.ablation_final --dry_run --init
+# dora run -f 54bff665
+# dora run -f 9da83e05
+# dora run -f 44c86253
+# dora run -f fcdf6773
+# dora run -f 8c6a0d2a
+# dora run -f 75eb4e8e
+# dora run -f a7b2007e
+# dora run -f a6920072
+# dora run -f 3c2ea57d
+# dora run -f 19ebcbd4
+# dora run -f 06d28863
+# dora run -f 41aaaa37
+dora run -f 5c28e97a
+dora run -f 7b67632d
+dora run -f 7d2880e1
+dora run -f adb1817e
+dora run -f 0ad5ed7c
+dora run -f 06cdd6cd
+dora run -f 93d54fbb
+dora run -f 773e5baf
+dora run -f 9b193586
+dora run -f e3cb2a32
+dora run -f 7a0ba64f
+dora run -f 8e173ee1
+dora run -f 958bc783
+dora run -f 2ca86e32
+dora run -f 6e0062ec
+dora run -f 62c651d2
+dora run -f efeafa59
+dora run -f e5124651
+dora run -f fde12fa8
+dora run -f 289aa972
+dora run -f 204d9479
+dora run -f 5140ecb9
+dora run -f ab5adadf
+dora run -f f5dfe325
+dora run -f 608502c9
+dora run -f 84bbc22d
+dora run -f c485c151
+dora run -f d186464f
+dora run -f f25f2b84
+dora run -f 761dacbc
+dora run -f aa88fddf
+dora run -f 167d9a46
+dora run -f 491090f8
+dora run -f b4b193b7
+dora run -f 8f3f1e59
+dora run -f b2a80101
+# dora run -f a83da97c
+# dora run -f 21a508a8
+# dora run -f b3b81be7
+# dora run -f f269407a
+# dora run -f ab36a8d6
+# dora run -f 7a233760
+# dora run -f 3bf39347
+# dora run -f e7b907b9
+# dora run -f 3c317326
+# dora run -f 032a91da
+# dora run -f 501a6aa2
+# dora run -f ab86d220
+dora run -f dc7c8e29
+dora run -f 82e8ecdc
+dora run -f 44df5534
+dora run -f 592a324e
+dora run -f 28f55b7a
+dora run -f 47eddd71
+dora run -f 03b59c2c
+dora run -f 05ef4ef9
+dora run -f 3fe20f39
+dora run -f 9d6c061b
+dora run -f 31e205b3
+dora run -f 12ebe52a
+dora run -f d87df22f
+dora run -f 734b80ed
+dora run -f a33cf1d8
+dora run -f 7bcefc09
+dora run -f deb5bf31
+dora run -f 0eb94e47
+dora run -f 0e13f469
+dora run -f de4c82b8
+dora run -f 93f3b5e0
+dora run -f 6bb0adf6
+dora run -f 1c3c3b5e
+dora run -f 791c81cf
+dora run -f 5b097d5d
+dora run -f eb7aa9f1
+dora run -f aa92101a
+dora run -f 2bd603eb
+dora run -f ecc28272
+dora run -f 9cd03397
+dora run -f 3bd5b03f
+dora run -f 6d4135ed
+dora run -f 636c1461
+dora run -f 4adbdc0f
+dora run -f 11023404
+dora run -f 571551d8
+# dora run -f 9e03e490
+# dora run -f 04a70d2e
+# dora run -f d83731bd
+# dora run -f c0c59a15
+# dora run -f c6747294
+# dora run -f 22db6ccf
+# dora run -f 08e706f7
+# dora run -f 0ff5723c
+# dora run -f aabd1222
+# dora run -f b9a15246
+# dora run -f 53bed631
+# dora run -f cfcef140
+dora run -f 1f701ecc
+dora run -f 54dc6b1b
+dora run -f 3270cd0f
+dora run -f 34c4a72d
+dora run -f 864466c4
+dora run -f 72241ac3
+dora run -f 4aab9b37
+dora run -f 5e8030b1
+dora run -f 139ed5d9
+dora run -f f487d36a
+dora run -f ba6239f7
+dora run -f 054e2a78
+dora run -f 84bf4b74
+dora run -f d08cee37
+dora run -f d63b59e8
+dora run -f 6339b53b
+dora run -f 55502e86
+dora run -f 794779cd
+dora run -f 3f52f6c4
+dora run -f 1b333dcc
+dora run -f 6ae5b3ef
+dora run -f a7d562e5
+dora run -f 9b6aeae6
+dora run -f caaa5442
+dora run -f cbad7317
+dora run -f 44001752
+dora run -f 47532c08
+dora run -f 58ed6e41
+dora run -f 79ba4c78
+dora run -f 56fc0cdb
+dora run -f 10deef7e
+dora run -f 729e9d07
+dora run -f 76b9b408
+dora run -f 750b1a82
+dora run -f a4f0d9b8
+dora run -f 8721ee4e
+dora grid nmi.ablation_final
+dora grid nm.ablation_final --dry_run
+python -m scripts.run_eval_probs grid_name="nmi.ablation_final"
