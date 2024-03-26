c951011e2a8eb59d1e9afcde4e0bf09a64d89652
Prateek-Karn <prateek.karn@yahoo.com>
2024-03-26 09:07:16 +0530

    Create batchtrain.py

---
+++ bm/grids/batchtrain.py
@@ -0,0 +1,33 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Ablation grid."""
+from itertools import product  # noqa
+from .._explorers import ClipExplorer
+from ..train import main  # noqa
+
+@ClipExplorer
+def explorer(launcher):
+    launcher.slurm_(
+        gpus=2, mem_per_gpu=200,
+        partition="learnlab",
+    )
+    # See conf/model/clip_conv.yaml for the configuration used.
+    launcher.bind_({'model': 'clip_conv', 'optim.batch_size': 16})
+    total_recordings=300
+    batch_size = 20
+    seeds = [2036, 2037, 2038]
+    audio_sets = ['gwilliams2022']
+    with launcher.job_array():
+        sub = launcher.bind({'dset.selections': [dset]}, seed=seed)
+        for seed, dset in product(seeds, audio_sets):
+            #the starting model
+            sub({"dset.n_recordings": batch_size})
+            for batch in range(batch_size,total_recordings,batch_size):
+                #the continuing model
+                xp = main.get_xp(sub._argv)
+                sub({"dset.n_recordings":batch_size,"dset.skip_recordings":batch},continue_sig=xp.sig,continue_best=False)
+            
