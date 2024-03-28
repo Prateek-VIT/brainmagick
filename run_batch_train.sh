6c8e19be096c1652260575c82eedd72392a64b10
Prateek-VIT <prateek.karn2020@vitstudent.ac.in>
2024-03-28 10:24:43 +0530

    final fix for total recordings

--- run_batch_train.sh
+++ run_batch_train.sh
@@ -9,11 +9,6 @@ dora run -f d34306cc
 dora run -f 1eae627c
 dora run -f a54072b2
 dora run -f b4078585
-dora run -f d6f92779
-dora run -f de15c0a3
-dora run -f 12b7d486
-dora run -f dc6c8466
-dora run -f 6b0ee0f1
 dora grid batchtrain
 dora grid batchtrain --dry_run
 python -m scripts.run_eval_probs grid_name="batchtrain"
