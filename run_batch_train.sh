45ba30e264e60c57b0bb301ea2d1309ecbcc1b3a
Prateek-VIT <prateek.karn2020@vitstudent.ac.in>
2024-03-28 13:57:18 +0530

    update the signatures in the batchtrain script

--- run_batch_train.sh
+++ run_batch_train.sh
@@ -1,14 +1,14 @@
 dora grid batchtrain --dry_run --init
-dora run -f 9317b135
-dora run -f 94ac7553
-dora run -f 06fc662f
-dora run -f 07072bb8
-dora run -f 99785ab8
-dora run -f 7115b243
-dora run -f d34306cc
-dora run -f 1eae627c
-dora run -f a54072b2
-dora run -f b4078585
+dora run -f 61d399d0
+dora run -f 0964ffe2
+dora run -f c76b236e
+dora run -f 6c92d4ce
+dora run -f f6647a23
+dora run -f 20005732
+dora run -f b1928e5e
+dora run -f a7535352
+dora run -f 5548e846
+dora run -f 26c87938
 dora grid batchtrain
 dora grid batchtrain --dry_run
 python -m scripts.run_eval_probs grid_name="batchtrain"
