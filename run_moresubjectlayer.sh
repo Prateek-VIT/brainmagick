2fd95d5edd169406c42c902e12caaced3b55ed95
Prateek-VIT <prateek.karn2020@vitstudent.ac.in>
2024-04-09 15:56:55 +0530

    fix some typo in shell script

--- run_moresubjectlayer.sh
+++ run_moresubjectlayer.sh
@@ -1,4 +1,4 @@
-dora grid batchtrain --dry_run --init
+dora grid moresubjectlayer --dry_run --init
 dora run -f 116083b6
 dora run -f 51e9e990
 dora run -f 483a4762
@@ -9,6 +9,6 @@ dora run -f 0d979517
 dora run -f ee7cfd8c
 dora run -f 0ffcb475
 dora run -f 6130bca6
-dora grid batchtrain
-dora grid batchtrain --dry_run
+dora grid moresubjectlayer
+dora grid moresubjectlayer --dry_run
 python -m scripts.run_eval_probs grid_name="moresubjectlayer"
