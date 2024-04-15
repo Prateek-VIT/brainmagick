6dabab1e95c3e451b92e856264932b8b3267ef0a
Prateek-VIT <prateek.karn2020@vitstudent.ac.in>
2024-04-15 10:59:43 +0530

    add a recurrence train runner script

---
+++ run_recurrencetrain.sh
@@ -0,0 +1,14 @@
+dora grid RecurrenceTrain --dry_run --init
+dora run -f db4df542
+dora run -f 56190304
+dora run -f 6be8d3d9
+dora run -f 448ee274
+dora run -f aff0c439
+dora run -f e35be78d
+dora run -f 4d087a0e
+dora run -f aacd3160
+dora run -f 39b76525
+dora run -f 9b02457b
+dora grid RecurrenceTrain
+dora grid RecurrenceTrain --dry_run
+python -m scripts.run_eval_probs grid_name="RecurrenceTrain"
