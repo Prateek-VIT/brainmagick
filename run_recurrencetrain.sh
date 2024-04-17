17915f8711d6494fd86b1fd9725149d45215479e
Prateek-Karn <prateek.karn@yahoo.com>
2024-04-18 01:45:39 +0530

    update exp sigs

--- run_recurrencetrain.sh
+++ run_recurrencetrain.sh
@@ -1,14 +1,14 @@
 dora grid RecurrenceTrain --dry_run --init
-dora run -f acc2ed18
-dora run -f 9a673fb9
-dora run -f aaaecb81
-dora run -f e1d33cf4
-dora run -f dd4c216a
-dora run -f d8ef50a5
-dora run -f 09649a95
-dora run -f 8f172b83
-dora run -f b139765e
-dora run -f 0718bb83
+dora run -f cee1ce25 
+dora run -f a95edc12 
+dora run -f c139b441 
+dora run -f 03713fbc 
+dora run -f ff1fc34d 
+dora run -f 0c6082d9 
+dora run -f daaeadad 
+dora run -f d8eaa124 
+dora run -f f86c83f5 
+dora run -f 62434dc2 
 dora grid RecurrenceTrain
 dora grid RecurrenceTrain --dry_run
 python -m scripts.run_eval_probs grid_name="RecurrenceTrain"
